ebc07b42346178dfe29e3f2cf218a1127c55475e
booiris <954520770@qq.com>
2021-10-08 21:11:47 +0800

    update

--- circle.scm
+++ circle.scm
@@ -1,7 +1,6 @@
 (define pi 3.14159)
 (define radius 10)
 (define circumference (* 2 pi radius))
-;(display (* pi (* radius radius)))
-;(begin (display circumference )
-;   (newline)) 
-circumference
+(display (* pi (* radius radius) ))  (newline)
+(display circumference ) (newline) 
+
