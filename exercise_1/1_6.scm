a400ca615e233ac82015e72c218472c0ba2865a2
booiris <954520770@qq.com>
2023-04-16 12:13:43 +0800

    feat: u

--- exercise_1/1_6.scm
+++ exercise_1/1_6.scm
@@ -5,14 +5,24 @@
   (cond (predicate then-clause)
         (else else-clause)))
 
+; Eva demonstrates the program for Alyssa:
+
 (new-if (= 2 3) 0 5)
 
+; 5
+
 (new-if (= 1 1) 0 5)
+
+; 0
+
+;Delighted, Alyssa uses new-if to rewrite the square-root program:
 
 (define (sqrt-iter guess x)
   (new-if (good-enough? guess x)
           guess
           (sqrt-iter (improve guess x)
                      x)))
+
+; What happens when Alyssa attempts to use this to compute square roots? Explain.
 
 ; 由于 normal-order 的原因，传入 new-if 的参数需要被计算一遍，导致 sqer-iter 重复迭代于同一个输入，递归爆栈。
--- exercise_1/1_7.scm
+++ exercise_1/1_7.scm
@@ -1,3 +1,61 @@
 ;Exercise 1.7.
 ;The good-enough? test used in computing square roots will not be very effective for finding the square roots of very small numbers. Also, in real computers, arithmetic operations are almost always performed with limited precision. This makes our test inadequate for very large numbers. Explain these statements, with examples showing how the test fails for small and large numbers. An alternative strategy for implementing good-enough? is to watch how guess changes from one iteration to the next and to stop when the change is a very small fraction of the guess. Design a square-root procedure that uses this kind of end test. Does this work better for small and large numbers?
 
+; (define (sqrt-iter guess x)
+;   (if (good-enough? guess x)
+;       guess
+;       (sqrt-iter (improve guess x) x)))
+
+; (define (improve guess x)
+;   (average guess (/ x guess)))
+
+; (define (average x y)
+;   (/ (+ x y) 2))
+
+; (define (good-enough? guess x)
+;   (< (abs (- (square guess) x)) 0.001))
+
+; (define (sqrt x)
+;   (sqrt-iter 1.0 x))
+
+; (sqrt 9)
+
+; ; test fails for small numbers
+; ; 
+; (sqrt 0.00009)
+
+; ; test fails for large numbers
+; ; time exceeded
+; (define x (sqrt 4598347981248908098))
+
+(define (sqrt-iter pre guess x)
+  (if (good-enough? pre guess)
+      guess
+      (sqrt-iter guess (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? pre guess)
+  (< (abs (- pre guess)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 0.0 1.0 x))
+
+(sqrt 9)
+; 3.000000001396984
+
+; test for small numbers
+; 
+(sqrt 0.00009)
+; 9.487978730289174e-3
+
+; test for large numbers
+(sqrt 4598347981248908098)
+; 2144375895.5110712
+
+(define x (sqrt 4598347981248908098))
+(square x)
