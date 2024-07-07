f19f856e59c7c13931c45baac3e36528fa83c0e7
booiris <booiris02@gmail.com>
2024-07-07 14:43:53 +0800

    update

--- exercise_1/1_6.rkt
+++ exercise_1/1_6.rkt
@@ -1,3 +1,5 @@
+#lang racket
+
 ;Exercise 1.6
 ;Alyssa P. Hacker doesn't see why if needs to be provided as a special form. ``Why can't I just define it as an ordinary procedure in terms of cond?'' she asks. Alyssa's friend Eva Lu Ator claims this can indeed be done, and she defines a new version of if:
 
@@ -20,9 +22,22 @@
 (define (sqrt-iter guess x)
   (new-if (good-enough? guess x)
           guess
-          (sqrt-iter (improve guess x)
-                     x)))
+          (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (* guess guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
 
 ; What happens when Alyssa attempts to use this to compute square roots? Explain.
 
-; 由于 normal-order 的原因，传入 new-if 的参数需要被计算一遍，导致 sqer-iter 重复迭代于同一个输入，递归爆栈。
+; 由于 applicative-order 的原因，显然 new-if 不支持惰性计算，传入 new-if 的参数需要被计算一遍，then-clause else-clause 都需要被计算，而 else-clause 中是一个递归自调用，导致出现死循环。
--- exercise_1/1_7.rkt
+++ exercise_1/1_7.rkt
@@ -1,32 +1,36 @@
+#lang racket
+
 ;Exercise 1.7.
 ;The good-enough? test used in computing square roots will not be very effective for finding the square roots of very small numbers. Also, in real computers, arithmetic operations are almost always performed with limited precision. This makes our test inadequate for very large numbers. Explain these statements, with examples showing how the test fails for small and large numbers. An alternative strategy for implementing good-enough? is to watch how guess changes from one iteration to the next and to stop when the change is a very small fraction of the guess. Design a square-root procedure that uses this kind of end test. Does this work better for small and large numbers?
 
-; (define (sqrt-iter guess x)
-;   (if (good-enough? guess x)
-;       guess
-;       (sqrt-iter (improve guess x) x)))
+(define (average x y)
+  (/ (+ x y) 2))
 
-; (define (improve guess x)
-;   (average guess (/ x guess)))
+(define (square x)
+  (* x x))
 
-; (define (average x y)
-;   (/ (+ x y) 2))
+(define (old-sqrt-iter guess x)
+  (if (old-good-enough? guess x)
+      guess
+      (old-sqrt-iter (old-improve guess x) x)))
 
-; (define (good-enough? guess x)
-;   (< (abs (- (square guess) x)) 0.001))
+(define (old-improve guess x)
+  (average guess (/ x guess)))
 
-; (define (sqrt x)
-;   (sqrt-iter 1.0 x))
+(define (old-good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
 
-; (sqrt 9)
+(define (old-sqrt x)
+  (old-sqrt-iter 1.0 x))
 
 ; ; test fails for small numbers
 ; Insufficient accuracy of the result
-; (sqrt 0.00009)
+; (old-sqrt 0.00009)
 
 ; ; test fails for large numbers
 ; ; time exceeded
-; (define x (sqrt 4598347981248908098))
+; 因为 lisp 实现的小数精度不足以表示两个大数之间的差，所以 sqrt 会陷入死循环而无法得出正确结果。
+; (define x (old-sqrt 4598347981248908098))
 
 (define (sqrt-iter pre guess x)
   (if (good-enough? pre guess)
@@ -36,26 +40,34 @@
 (define (improve guess x)
   (average guess (/ x guess)))
 
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (good-enough? pre guess)
+(define (good-enough-1? pre guess)
   (< (abs (- pre guess)) 0.001))
+
+(define (good-enough-2? pre guess)
+  (< (/ (abs (- pre guess)) pre) 0.001))
+
+(define good-enough? good-enough-1?)
 
 (define (sqrt x)
   (sqrt-iter 0.0 1.0 x))
 
 (sqrt 9)
-; 3.000000001396984
+; good-enough-1 3.000000001396984
+; good-enough-2 3.00009155413138
 
 ; test for small numbers
-; 
+;
 (sqrt 0.00009)
-; 9.487978730289174e-3
+; good-enough-1 0.009487978730289174
+; good-enough-2 0.009486833049684392
 
 ; test for large numbers
 (sqrt 4598347981248908098)
-; 2144375895.5110712
+; good-enough-1 2144375895.5110712
+; good-enough-2 2144376367.0839047
 
 (define x (sqrt 4598347981248908098))
+(println x)
 (square x)
+; good-enough-1 4598347981248909000
+; good-enough-2 4598350003707965400
